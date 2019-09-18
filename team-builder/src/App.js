175458b2ebf732760962faec1de871568f19beff
James Grantham <james.r.grantham@gmail.com>
2019-09-18 19:46:41 +0100

    Create state change functions

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -17,6 +17,30 @@ function App() {
   const [memberList, setMemberList] = useState(initialMembers);
   const [memberForm, setMemberForm] = useState();
 
+  const onTitleChange = e => {
+    setMemberForm({
+      title: e.target.value,
+      firstName: memberForm.firstName,
+      surname: memberForm.surname,
+    })
+  }
+
+  const onFirstNameChange = e => {
+    setMemberForm({
+      title: memberForm.title,
+      firstName: e.target.value,
+      surname: memberForm.surname,
+    })
+  }
+
+  const onSurnameChange = e => {
+    setMemberForm({
+      title: memberForm.title,
+      firstName: memberForm.firstName,
+      surname: e.target.value,
+    })
+  }
+
   return (
     <div className="App">
       <MemberForm />
@@ -34,13 +58,16 @@ export default App;
 
 function MemberForm(props) {
 
+  const { Title, firstName, surname } = props;
 
   return (
     <form>
       <label htmlFor='titleInput'> Title </label>
       <input id='titleInput' type='text'/>
+
       <label htmlFor='firstNameInput'> First Name </label>
       <input id='firstNameInput' type='text'/>
+
       <label htmlFor='surnameInput'> Surname </label>
       <input id='surnameInput' type='text'/>
 
